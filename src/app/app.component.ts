07b572674601cbef9d7bd2411c38958c269fde47
Gabriel Trossero <gabi_trossero@hotmail.com>
2022-07-15 12:57:40 -0300

    servicio con promesa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,4 +24,21 @@ export class AppComponent {
     this.personasService.create(new Persona('Veronica', 'Diaz', 66, true));
   }
 
+  //METODO TRADICIONAL PARA RECUPERAR LA PROMESA
+  onClickActivas(){
+    this.personasService.activosV2() //llamo al servicio. Como me devuelve una promesa hago lo siguiente:
+      .then(arrTempPersonas => console.log(arrTempPersonas))
+      .catch(error => console.log(error));
+  }
+
+  //METODO MODERNO PARA RECUPERAR LA PROMESA
+  async onClickActivasV2(){ //async: va en el ámbito donde se utilice el await    
+    try { //para capturar si se produce un error
+      this.arrPersonas = await this.personasService.activosV2(); //await: espera la ejecucion y luego haz las siguientes lineas que siguen
+    } catch(error) {
+      console.log(error);
+    }
+    
+  }
+
 }
--- src/app/servicios/personas.service.ts
+++ src/app/servicios/personas.service.ts
@@ -25,4 +25,28 @@ export class PersonasService {
   create(nuevaPersona: Persona){
     this.personas.push(nuevaPersona);
   }
+
+  //función que filtra a las personas activas
+  //una promesa no va a interrumpir el resto de los procesos que se ejecuten
+  activos(): Promise<Persona[]> { //la promesa se va a resolver en un array de personas
+    const prom = new Promise<Persona[]>((resolve, reject) => { //ponemos el tipo en concreto de lo que está resolviendo. Siempre recibe el metodo resolve y reject
+      const arrTemp: Persona[] = [];
+      for(let persona of this.personas){
+        if(persona.activo){
+          arrTemp.push(persona);
+        }
+      }
+      resolve(arrTemp);
+    });
+    return prom;
+  }
+
+  activosV2(): Promise<Persona[]> {
+    return new Promise((resolve, reject) => {
+      const arrTemp = this.personas.filter(persona => { //analizo si la persona pasa o no el filtro
+        return persona.activo; //ya directamente si es activo devuleve true
+      });
+      resolve(arrTemp);
+    })
+  }
 }
